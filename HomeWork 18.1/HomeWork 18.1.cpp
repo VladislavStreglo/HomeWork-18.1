fa40291ccfd1985d9bd3ac50373319e8caca68b9
Streglo Vladislav <streglo1994@gmail.com>
2023-10-13 11:31:20 +0300

    Создаем структуру "Работник"

--- HomeWork 18.1/HomeWork 18.1.cpp
+++ HomeWork 18.1/HomeWork 18.1.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+// Создаем структуру "Работник"
+struct Employee {
+    char lastName[50];
+    int birthYear;
+    char position[50];
+    double salary;
+    char education[50];
+};
+
 int main()
 {
     setlocale(0, "");
